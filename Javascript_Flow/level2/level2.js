b7ca38cb8a931a1d052cd80cdf6d411c584a79ec
lyeons <dlgpfus00@gmail.com>
2023-10-16 19:16:39 +0900

    js 삭제버튼 수정

--- Javascript_Flow/level2/level2.js
+++ Javascript_Flow/level2/level2.js
@@ -1,8 +1,42 @@
-import { RESERVATION_LIST } from './reservation .js';
+import { RESERVATION_LIST } from "./reservation .js";
 console.log(RESERVATION_LIST);
+
+// 1. 예약 고객 리스트는 js로 이미 import 되어 console.log에 찍어두었습니다.
+// 2. 고객 리스트에 없는 고객정보의 경우 일치하는 항목이 없습니다라는 console.log와 함께 텍스트가 나타납니다.
+// 3. 이름과 핸드폰 번호 모두 일치하지 않으면 고객은 검색할 수 없습니다.
+// 4. 고객데이터가 있으면 고객번호가 텍스트로 나타납니다v
 
 /* 
 예약 고객확인하기
-
-
 */
+
+const $form = document.querySelector("form");
+const $inputName = document.querySelector(".user-name");
+const $inputPhone = document.querySelector(".user-phone");
+const $btn = document.querySelector("button");
+const $reservationNum = document.querySelector("#reservation-number");
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+const findIndex = 0;
+
+$btn.addEventListener("click", () => {
+  RESERVATION_LIST.forEach((el) => {
+    console.log(el);
+    //const checkName = el.name.includes($inputName);
+
+    findIndex++;
+  });
+  //   for (let i = 0; i < RESERVATION_LIST.length; i++) {
+  //     const checkName = RESERVATION_LIST[i].name.includes($inputName); //포함되어있으면 true반환
+  //     const checkPhone = RESERVATION_LIST[i].phone.includes($inputPhone);
+
+  // if (RESERVATION_LIST[i].phone === $inputPhone.value) {
+  //   if (RESERVATION_LIST[i].name === $inputName.value) {
+  //     $reservationNum.innerHTML = RESERVATION_LIST[i].number;
+  //     check = 1;
+  //   }
+  // }
+  //   }
+});
--- Javascript_Flow/level3/level3.js
+++ Javascript_Flow/level3/level3.js
@@ -1,3 +1,65 @@
 /* 
 레시피 재료 추가하기
 */
+
+const $form = document.querySelector("#ingredient-form");
+const $inputIngredientName = document.querySelector("#input-ingredient-name");
+const $inputWeight = document.querySelector("#input-weight");
+const $th = document.querySelectorAll(".thead > th");
+const $submitBtn = document.querySelector("#submit_button");
+const $ingredientList = document.querySelector("#ingredient-list");
+
+const $addBtn = document.querySelector("#input-all");
+const $table = document.querySelector("table");
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+let ingredientArr = [];
+//저장버튼 이벤트
+$addBtn.addEventListener("click", () => {
+  const $tr = document.createElement("tr");
+  const inputIngredientName = $inputIngredientName.value;
+  const inputWeight = $inputWeight.value;
+
+  if (ingredientArr.includes(inputIngredientName)) {
+    alert("이미 존재하는 재료입니다.");
+    $inputIngredientName.value = "";
+    $inputWeight.value = "";
+  } else {
+    ingredientArr.push(inputIngredientName);
+    const $td = document.createElement("td");
+
+    const arr = [inputIngredientName, inputWeight];
+
+    for (let i = 0; i < $th.length - 1; i++) {
+      $tr.innerHTML += `<td>${arr[i]}</td>`;
+    }
+
+    $table.append($tr);
+    $tr.append($td);
+
+    const $delBtn = document.createElement("button");
+    $delBtn.className = "delete-btn";
+    $delBtn.innerText = "삭제";
+    $td.append($delBtn);
+
+    $tr.className = "thead";
+
+    $inputIngredientName.value = "";
+    $inputWeight.value = "";
+
+    $delBtn.addEventListener("click", (e) => {
+      e.target.parentNode.parentNode.remove();
+      const delTarget = e.target.parentNode.parentNode.innerText;
+      for (let i = 0; i <= ingredientArr.length; i++) {
+        if (ingredientArr[i] === delTarget[0]) {
+          ingredientArr.splice(i, 1);
+        }
+      }
+    });
+  }
+});
+
+$submitBtn.addEventListener("click", () => {});
